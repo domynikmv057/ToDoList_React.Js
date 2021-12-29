b4e9dd072c2d3ee930afc20e5c12398ed54e3c81
domynikmv057 <domynikmv.057@gmail.com>
2021-12-29 16:44:29 -0300

    add addToDoItem Fuction

--- src/App.jsx
+++ src/App.jsx
@@ -1,141 +1,17 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
-import { RiPencilFill } from 'react-icons/ri';
-import { FaTrash } from 'react-icons/fa';
-import { AiOutlineCheck } from 'react-icons/ai';
 import './stles.css';
 import logEmix from './imagens/logoEmix.png';
 import ToDoBox from './Components/ToDoBox/ToDoBox';
-
-
+import {  v5 } from 'uuid'
+import { v4 } from 'uuid';
 
 function App() {
 	let [toDoLista, setToDoLista] = useState([]);
 	let [novoToDo, setNovoToDo] = useState('');
 	let [editToDo, setEditToDo] = useState([]);
 
-	useEffect(() => {
-		const localStorageRepo = localStorage.getItem('toDoLista');
-		if (localStorageRepo) {
-			setToDoLista(JSON.parse(localStorageRepo));
-		}
-	}, []);
 
-	useEffect(() => {
-		localStorage.setItem('toDoLista', JSON.stringify(toDoLista));
-	}, [toDoLista]);
-
-	return (
-		<>
-			<nav>
-				<div>
-					<p>
-						<span>TODO</span> LIST
-					</p>
-				</div>
-				<div>
-					<img src={logEmix} alt="logo" width="140px" />
-				</div>
-			</nav>
-			<main>
-        <ToDoBox title="A fazer" itemList={toDoLista} />
-				<div className="addTasck">
-					<input
-						value={novoToDo}
-						onChange={(valChange) => setNovoToDo(valChange.target.value)}
-						type="text"
-						placeholder="Digite aqui uma nova tarefa..."
-					/>
-					<button onClick={() => addItenToDo()}>ADICIONAR</button>
-				</div>
-				<div className="tasksContainer">
-					<h3> A fazer </h3>
-					<ul className="toDoS">
-						{toDoLista.map((item, index) => {
-							if (item.checked === false) {
-								return (
-									<li key={item.id}>
-										<div className="liStyle list_icons_style">
-											<div className="inputTasks">
-												<input
-													className="checkStyle"
-													type="checkbox"
-													onChange={(event) => toDoChecked(index, event, item)}
-													checked={item.checked}
-												/>
-												{item.isEdit ? (
-													<input
-														className="editingIput"
-														value={editToDo.desc}
-														onChange={(valChange) =>
-															setEditToDo(valChange.target.value)
-														}
-													></input>
-												) : (
-													<span>{item.desc}</span>
-												)}
-											</div>
-											<div className="buttonTasks">
-												{item.isEdit ? (
-													<button onClick={() => salvarToDo(index, item)}>
-														<AiOutlineCheck />
-													</button>
-												) : (
-													<button onClick={() => editToDos(index, item)}>
-														<RiPencilFill />
-													</button>
-												)}
-												<button onClick={() => deletarToDos(index)}>
-													<FaTrash />
-												</button>
-											</div>
-										</div>
-									</li>
-								);
-							}
-						})}
-					</ul>
-				</div>
-
-				<div className="tasksContainer">
-					<h3> Feito</h3>
-					<ul>
-						{toDoLista.map((item, index) => {
-							if (item.checked === true) {
-								return (
-									<li key={item.id}>
-										<div className="liStyle list_icons_style completTask">
-											<input
-												className="checkStyle"
-												type="checkbox"
-												onChange={(event) => toDoChecked(index, event, item)}
-												checked={item.checked}
-											/>
-											<span>{item.desc}</span>
-											<button onClick={() => deletarToDos(index)}>
-												<FaTrash />
-											</button>
-										</div>
-									</li>
-								);
-							}
-						})}
-					</ul>
-				</div>
-			</main>
-		</>
-	);
-
-	function addItenToDo(index) {
-		let corpoToDo = {
-			id: idAleatorio(),
-			desc: novoToDo,
-			checked: false,
-			isEdit: false,
-		};
-		setToDoLista([...toDoLista, corpoToDo]);
-		setNovoToDo('');
-	}
 
 	function toDoChecked(index, event, item) {
 		let toDoListTemp = [...toDoLista];
@@ -174,6 +50,60 @@ function App() {
 		const max = 999;
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
+
+
+	function addToDoItem (text) {
+		setToDoLista([...toDoLista, {
+			id: v4(),
+			desc: text ,
+			checked: false,
+			isEdit: false
+		}])
+		setNovoToDo("")
+	}
+
+	useEffect(() => {
+		const localStorageRepo = localStorage.getItem('toDoLista');
+		if (localStorageRepo) {
+			setToDoLista(JSON.parse(localStorageRepo));
+		}
+	}, []);
+
+	useEffect(() => {
+		localStorage.setItem('toDoLista', JSON.stringify(toDoLista));
+	}, [toDoLista]);
+
+
+
+	return (
+		<>
+			<nav>
+				<div>
+					<p>
+						<span>TODO</span> LIST
+					</p>
+				</div>
+				<div>
+					<img src={logEmix} alt="logo" width="140px" />
+				</div>
+			</nav>
+			<main>
+				<div className="addTasck">
+					<input
+						value={novoToDo}
+						onChange={(valChange) => setNovoToDo(valChange.target.value)}
+						type="text"
+						placeholder="Digite aqui uma nova tarefa..."
+					/>
+					<button onClick={() => addToDoItem(novoToDo)}>ADICIONAR</button>
+				</div>
+				<ToDoBox title="A fazer" itemList={toDoLista.filter(item => !item.checked)} />
+
+				<ToDoBox title="Feito" itemList={toDoLista.filter(item => item.checked) } />
+
+			</main>
+		</>
+	);
 }
 
 export default App;
--- src/Components/ToDoBox/ToDoBox.jsx
+++ src/Components/ToDoBox/ToDoBox.jsx
@@ -2,13 +2,18 @@ import './ToDoBox.css';
 import UncheckedToDo from '../UncheckedToDo/UncheckedToDo';
 import CheckedToDo from '../CheckedToDo/CheckedToDo';
 
-
 function ToDoBox({ title, itemList }) {
 	return (
 		<section className="box-section">
 			<h3> {title} </h3>
 			<ul>
-				{itemList.map((item) => item.checked ? <CheckedToDo text={ item.desc } /> : <UncheckedToDo text={ item.desc } /> )}
+				{itemList.map((item) =>
+					item.checked ? (
+						<CheckedToDo text={item.desc} />
+					) : (
+						<UncheckedToDo text={item.desc} />
+					),
+				)}
 			</ul>
 		</section>
 	);
